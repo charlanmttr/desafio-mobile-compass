a7cab81b466f48d1d88facda40136ce05b134341
Charlan Matter <mattergremio2@gmail.com>
2022-10-12 21:06:15 -0300

    configuring Stack Navigator and routes

---
+++ src/homepage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function Homepage() {
+  return (
+    <View>
+      <Text>Homepage</Text>
+    </View>
+  );
+}
+
+export default Homepage;
