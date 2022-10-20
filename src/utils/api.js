64f817aced37e91aa85f7166144942a9ad7ee766
Charlan Matter <mattergremio2@gmail.com>
2022-10-20 17:39:07 -0300

    configuring axios to requests on api

---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://bank-statement-bff.herokuapp.com',
+  headers: {
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+  },
+});
